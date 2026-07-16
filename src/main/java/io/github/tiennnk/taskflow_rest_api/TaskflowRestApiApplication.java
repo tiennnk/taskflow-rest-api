9ce5aa7122f900817ef7639757613a69a91a6334
tiennnk <kieutiennguyenngoc2002.tn@gmail.com>
2026-07-17 00:43:46 +0700

    [init] init Spring Boot project

---
+++ src/main/java/io/github/tiennnk/taskflow_rest_api/TaskflowRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.tiennnk.taskflow_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowRestApiApplication.class, args);
+	}
+
+}
